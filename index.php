db2092e1c7cd651f3399a6dfc50ddccd9d0e0e27
JassRg <65271820+JassRg@users.noreply.github.com>
2020-05-13 00:06:17 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method = "POST"){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hola':
+			speech = "Hola buenas tardes";
+			break;
+		case 'adios':
+			speech = "Gracias por hablar con el chatbot";
+			break;
+		case 'info':
+			speech = "Por el momento no hay opciones";
+			break;
+
+		default:
+			speech = "Perdon, podrías repetirlo?";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response-> speech = "";
+	$response -> displayText = "";
+	$response -> source = "webhook";
+	echo json_encode($response);
+
+
+}
+else{
+	echo "Method not allowed";
+}
+
+?>
